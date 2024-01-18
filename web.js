e57faf31cbf382e391843689f6e8766fbb68a908
Lavanya Sree P <155654797+Lavanyasree8323@users.noreply.github.com>
2024-01-18 22:14:46 +0530

    Add files via upload

---
+++ web.js
@@ -0,0 +1,24 @@
+let h;
+function HTML() {
+    h=window.open("http://127.0.0.1:5500/PROJECT/website/htmla.html","_blank","width=300,height=200")
+}
+let qsa;
+function qs() {
+    qsa=window.open("http://127.0.0.1:5500/PROJECT/website/q.html","_blank","width=300,height=200")
+}
+let add;
+ function adv() {
+    add=window.open("http://127.0.0.1:5500/PROJECT/website/a.html","_blank","width=300,height=200")
+}
+let cf;
+function cfb() {
+    cf=window.open("http://127.0.0.1:5500/PROJECT/website/cfb.html","_blank","width=300,height=200")
+}
+let no;
+ function NOB() {
+    no=window.open("http://127.0.0.1:5500/PROJECT/website/BOMNA.html","_blank","width=300,height=200")
+}
+let SO;
+function sob() {
+    SO=window.open("http://127.0.0.1:5500/PROJECT/website/BOMCA.HTML","_blank","width=300,height=200")
+}
